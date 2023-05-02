8d5aa5ac30809ddebbf4e501597d5661eea37a7b
Aditya <adityasinghsmpr786@gmail.com>
2023-05-02 11:10:31 +0530

    Some css changes

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,51 +1,57 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Spinner from "./Spinner";
+import useGif from "../hooks/useGif";
 
 //here we take our api from env file
 const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
 const Random = () => {
-  const [gif, setGif] = useState('');
-  const [loading, setLoading] = useState(false);
+//   const [gif, setGif] = useState('');
+//   const [loading, setLoading] = useState(false);
 
-  //   api call
+//   //   api call
 
-  async function fetchData() {
-    setLoading(true);
-    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
-    const {data} = await axios.get(url);
-    console.log(data)
-    const imageSource = data.data.images.downsized_large.url;
+//   async function fetchData() {
+//     setLoading(true);
+//     const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+//     const {data} = await axios.get(url);
+//     console.log(data)
+//     const imageSource = data.data.images.downsized_large.url;
 
-    //console.log(imageSource)
-    setGif(imageSource);
-    setLoading(false);
-  }
+//     //console.log(imageSource)
+//     setGif(imageSource);
+//     setLoading(false);
+//   }
 
-  useEffect( () => {
-    fetchData();
-  },[] );
+//   useEffect( () => {
+//     fetchData();
+//   },[] );
 
-  function clickHandler() {
-    fetchData();
-    console.log("button clixked");
-  }
+
+//we use our cusom hook
+
+const{gif, loading, fetchData} =useGif();
+
+//   function clickHandler() {
+//     fetchData();
+//     //console.log("button clicked");
+//   }
 
   return (
     <div
       className="w-1/2 bg-green-400  rounded-xl border border-black
-    flex flex-col items-center gap-y-5 mt-[15px]"
+    flex flex-col items-center gap-y-5 mt-[15px] "
     >
       <h1 className="text-2xl underline uppercase font-bold mt-[15px]">A Random Gif</h1>
       {
-        loading ? (<Spinner></Spinner>):(<img src={gif} alt="randomGif" width="450" />)
+        loading ? (<Spinner></Spinner>):(<img src={gif} alt="randomGif" width="450" className="rounded-md" />)
       }
 
       
 
       <button
-        onClick={clickHandler}
+        onClick={()=>fetchData()}
         className="w-9/12 bg-white text-lg py-2 rounded-lg mb-[20px]"
       >
         Generate
--- src/components/Tag.js
+++ src/components/Tag.js
@@ -46,7 +46,7 @@ const{gif, loading, fetchData} = useGif(tag);
   return (
     <div
       className="w-1/2 bg-blue-400  rounded-xl border border-black
-    flex flex-col items-center gap-y-5 mt-[15px]"
+    flex flex-col items-center gap-y-5 mt-[15px] mb-[20px]"
     >
       <h1 className="text-2xl underline uppercase font-bold mt-[15px]">
         Random {tag} Gif
@@ -54,7 +54,7 @@ const{gif, loading, fetchData} = useGif(tag);
       { loading ? (
         <Spinner/>
       ) : (
-        <img src={gif} alt="randomGif" width="450"/>
+        <img src={gif} alt="randomGif" width="400" className="rounded-md"/>
       )}
 
         
